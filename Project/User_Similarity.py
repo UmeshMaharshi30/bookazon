3638b9e0d1805713b1c6fff3e49aee002bcd3f49
Anurag <anuragdesai67@gmail.com>
2018-12-04 12:06:39 -0600

    cosine similarity for users

--- Project/User_Similarity.py
+++ Project/User_Similarity.py
@@ -4,6 +4,7 @@ import csv
 from collections import defaultdict
 from collections import Counter
 import pandas as pd
+from scipy import spatial
 
 def import_ratings_data(filename):
     ratings_file = pd.read_csv(filename, header=0)
@@ -20,10 +21,12 @@ file_contents = import_ratings_data('C:/Users/anurag/Desktop/Project572/bookazon
 books_read_by_user = set({})
 
 def get_books_rated_by_User(userid):
+    books_and_ratings_by_user = defaultdict(dict)
     for row in file_contents: # read a row as {column1: value1, column2: value2,...}
         if(row[1] == userid):
             books_read_by_user.add(row[0])
-    return books_read_by_user
+            books_and_ratings_by_user[row[1]][row[0]] = row[2]
+    return books_read_by_user, books_and_ratings_by_user
     
 def get_users_rated_same_books(books_read_by_user):
     related_users = defaultdict(dict)
@@ -32,7 +35,26 @@ def get_users_rated_same_books(books_read_by_user):
             related_users[row[1]][row[0]] = row[2]
     return related_users
 
-books_read_by_user = get_books_rated_by_User(17329)
+def cosine_similarity(user1, user2):
+    data1 = []
+    data2 = []
+    user2_book_ratings = related_users[user2]
+    for key in user2_book_ratings:
+        data2.append(related_users[user2][key])
+        data1.append(books_and_ratings_by_user[user1][key])
+    result = 1 - spatial.distance.cosine(data1, data2)
+    return result
+
+user1 = 17329
+books_read_by_user, books_and_ratings_by_user = get_books_rated_by_User(user1)
 related_users = get_users_rated_same_books(books_read_by_user)
 print(len(related_users))
+user_similarity_score = {}
+for user2 in related_users:
+    result = cosine_similarity(user1, user2)
+    user_similarity_score[user2] = result
+
+import heapq
+top5 = heapq.nlargest(5, user_similarity_score, key=user_similarity_score.get)
+print(top5)
 
