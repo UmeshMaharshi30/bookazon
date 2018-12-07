1558fe24f3253526703061b81959d1fc6229a846
Anurag <anuragdesai67@gmail.com>
2018-12-06 20:38:22 -0600

    update

--- Project/book_similarity.py
+++ Project/book_similarity.py
@@ -1,125 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 02 17:42:55 2018
+Created on Mon Nov 19 17:42:55 2018
 
-@author: Giang
+@author: umesh
 """
 
 # -*- coding: utf-8 -*-
 
-
+from os import system
 import pandas as pd
-import numpy as np
-from scipy.sparse import csr_matrix
-import matplotlib.pyplot as plt
+import numpy
 
-books = pd.read_csv('../Cleansed/Final.csv', sep=',', error_bad_lines=False, encoding="latin-1")
-books.columns = ['id', 'book_id', 'authors', 'original_publication_year', 'original_title', 'language_code',
-                      'average_rating', 'ratings_count', '']
-
-ratings = pd.read_csv('../data set/ratings.csv', sep=',', error_bad_lines=False, encoding="latin-1")
-ratings.columns = ['id', 'user_id', 'rating']
-
-categories = pd.read_csv('../Cleansed/book_tags.csv', sep=',', error_bad_lines=False, encoding="latin-1")
-categories.columns = ['book_id', 'tag_name']
-
-
-combine_book_rating = pd.merge(ratings, books, on='id')
-columns = ['book_id', 'book_id', 'authors', 'language_code', 'original_publication_year', 'average_rating', '']
-
-combine_book_rating = combine_book_rating.drop(columns, axis=1)
-print(combine_book_rating.head())
-
-
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-print(combine_book_rating['ratings_count'].describe())
-
-print(combine_book_rating['ratings_count'].quantile(np.arange(.8, 1, .01)))
-
-popularity_threshold = 60000
-rating_popular_book = combine_book_rating.query('ratings_count >= @popularity_threshold')
-rating_popular_book.head()
-
-
-rating_popular_book = rating_popular_book.drop_duplicates(['user_id', 'original_title'])
-print(rating_popular_book.head())
-rating_popular_book_pivot = rating_popular_book.pivot(index = 'original_title', columns = 'user_id', values = 'rating').fillna(0)
-rating_popular_book_matrix = csr_matrix(rating_popular_book_pivot.values)
-print(rating_popular_book_matrix)
-
-from sklearn.neighbors import NearestNeighbors
-
-model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
-model_knn.fit(rating_popular_book_matrix)
-
-query_index = np.random.choice(rating_popular_book_pivot.shape[0])
-distances, indices = model_knn.kneighbors(rating_popular_book_pivot.iloc[query_index, :].reshape(1, -1), n_neighbors = 6)
-
-for i in range(0, len(distances.flatten())):
-    if i == 0:
-        print('Recommendations for {0}:\n'.format(rating_popular_book_pivot.index[query_index]))
-    else:
-        print('{0}: {1}, with distance of {2}:'.format(i, rating_popular_book_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
-
-
-'''
-book_info = "../data set/book_info.csv"
-
-book_tag_file = "../data set/book_tags.csv"
-
-book_file = "../data set/books.csv"
-
-rating = "../data set/ratings.csv"
-
-book_info_cleansed = "../giang/book_info_cleansed.csv"
-genres = ["Art", "Biography", "Business", "Christian", "Classics", "Comics", "Cookbooks", "Crime", "Fantasy", "Fiction",
-          "Historical Fiction", "History", "Manga", "Mystery", "Poetry", "Psychology"]
-
-auth_cols = ["author", "Art", "Biography", "Business", "Christian", "Classics", "Comics", "Cookbooks", "Crime",
-             "Fantasy", "Fiction", "Historical Fiction", "History", "Manga", "Mystery", "Poetry", "Psychology"]
-
-cleaned_tags = []
-
-author_list = []
-
-def author_name_preprocessing():
-    authors = pd.read_csv(book_info, usecols=["authors"])
-    for author in authors.iterrows():
-        #print(author[1].authors)
-        count = 0
-        for i in range(len(author[1].authors)):
-            if author[1].authors[i] is ',':
-                count += 1
-
-        author_list.append([author[1].authors.split(',')[j].lstrip(' ') for j in range(count+1)])
-
-    print(author_list)
-
-    with open(book_info_cleansed, 'w', newline='', encoding="utf8") as writeFile:
-        writer = csv.writer(writeFile)
-        #for i in range(len(author_list)):
-        writer.writerows(author_list)
+# Load book attributes
+columns = ["id","book_id","authors","original_publication_year","original_title","language_code","average_rating","ratings_count"];
+book_data_file = "../Cleansed/Final.csv";
+heuristic_func = [0,0,10,3,0,2,4,0];
+similarity_file = "similarity.csv";
+book_tag_file = "../data set/book_tags.csv";
 
 
 
-author_name_preprocessing()
-'''
+def get_all_books():
+    books = pd.read_csv(book_data_file, usecols=range(0,8));
+    # top 20 data
+    #print(books.loc[0]);
+    return books;
 
-'''
 
-plt.rc("font", size=15)
-categories.tag_name.value_counts(sort=False).plot(kind='bar')
-plt.title('Categories Distribution\n')
-plt.xlabel('Categories')
-plt.ylabel('Count')
-plt.savefig('system1.png', bbox_inches='tight')
-plt.show()
+def calculate_similarity():
+    books = get_all_books();
+    total_books = len(books);
+    book_sim = numpy.zeros((total_books, total_books));
+    total_attr = len(heuristic_func);
+    book_tags = setup_tags();
+    for row in range(0, total_books):
+        for col in range(row + 1, total_books):
+            b = books.loc[col];
+            match_book = books.loc[row];
+            heu_val = 0;
+            if row != col: 
+                for attr in range(0,total_attr):
+                    if attr == 6:
+                        heu_val = heu_val + (heuristic_func[attr] * match_book[columns[attr]]);
+                    elif b[columns[attr]] == match_book[columns[attr]]:
+                        heu_val = heu_val + heuristic_func[attr];
+                    #print(heu_val);    
+            book_sim[row][col] = heu_val + get_tag_match_value(book_tags, row, col);
+        print("row", row, "done");    
+    df = pd.DataFrame(book_sim)
+    df.to_csv(similarity_file, header=None);
+        
 
-plt.rc("font", size=15)
-ratings.rating.value_counts(sort=False).plot(kind='bar')
-plt.title('Rating Distribution\n')
-plt.xlabel('Rating')
-plt.ylabel('Count')
-plt.savefig('system1.png', bbox_inches='tight')
-plt.show()
-'''
+def setup_tags():
+    book_tags = pd.read_csv(book_tag_file);
+    #print(book_tags)
+    #print(book_tags.loc[book_tags["goodreads_book_id"] == 1]);
+    return book_tags;
+
+
+def get_tag_match_value(all_book_tags, row_book, col_book):
+    total_col_tags = all_book_tags.loc[all_book_tags["goodreads_book_id"] == col_book + 1];
+    total_row_tags = all_book_tags.loc[all_book_tags["goodreads_book_id"] == row_book + 1];
+    matched = 0;
+    if (len(total_col_tags) > 0 and len(total_row_tags) > 0):
+        for index, row in total_col_tags.iterrows():
+            if len(total_row_tags.loc[total_row_tags["tag_id"] == row["tag_id"]]) > 0:
+                matched = matched + 1;
+    if(len(total_col_tags) > 0):
+        matched = ((matched)/len(total_col_tags))*10;        
+    return matched;        
+
+def main():
+    system('clear');
+    #get_all_books();
+    #setup_tags();
+    calculate_similarity();
+
+
+if __name__ == '__main__':
+    main()     
